aa7db9ed4f900a17d5091fcefa9907d578e58918
Josh Aragon <josharagon99@gmail.com>
2022-09-13 12:07:35 -0600

    Hard code store id

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,12 +1,6 @@
 export const fetchSearchQuery = async (search, page) => {
-  const url = `https://api.searchspring.net/api/search/search.json?siteId=${process.env.REACT_APP_SITE_ID}&q=${search}&resultsFormat=native&page=${page}`;
-  return await fetch(url, {
-    headers: {
-      "Content-Type": "application/json;charset=UTL-8",
-      "Access-Control-Allow-Origin": "*",
-      Accept: "application/json",
-    },
-  })
+  const url = `https://api.searchspring.net/api/search/search.json?siteId=scmq7n&q=${search}&resultsFormat=native&page=${page}`;
+  return await fetch(url)
     .then((res) => res.json())
     .then((searchResult) => {
       console.log(searchResult);
