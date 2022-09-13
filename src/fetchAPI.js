30bfcea7cb076aeaf15ebb86efdf479de132f023
Josh Aragon <josharagon99@gmail.com>
2022-09-13 12:00:15 -0600

    Header test

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,6 +1,12 @@
 export const fetchSearchQuery = async (search, page) => {
   const url = `https://api.searchspring.net/api/search/search.json?siteId=${process.env.REACT_APP_SITE_ID}&q=${search}&resultsFormat=native&page=${page}`;
-  return await fetch(url)
+  return await fetch(url, {
+    headers: {
+      "Content-Type": "application/json;charset=UTL-8",
+      "Access-Control-Allow-Origin": "*",
+      Accept: "application/json",
+    },
+  })
     .then((res) => res.json())
     .then((searchResult) => {
       console.log(searchResult);
