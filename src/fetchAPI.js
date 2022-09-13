3dbaaf81edefaa674e090629b3ac46b3df9c92e0
Josh Aragon <josharagon99@gmail.com>
2022-09-13 11:15:11 -0600

    Remove headers

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,10 +1,6 @@
 export const fetchSearchQuery = async (search, page) => {
   const url = `https://api.searchspring.net/api/search/search.json?siteId=${process.env.REACT_APP_SITE_ID}&q=${search}&resultsFormat=native&page=${page}`;
-  return await fetch(url, {
-    headers: new Headers({
-      "Access-Control-Allow-Origin": "*",
-    }),
-  })
+  return await fetch(url)
     .then((res) => res.json())
     .then((searchResult) => {
       console.log(searchResult);
