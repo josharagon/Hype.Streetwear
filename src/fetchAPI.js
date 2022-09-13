42d4f04284a0c54b2fd25e25d7df1ecdcafc4eb0
Josh Aragon <josharagon99@gmail.com>
2022-09-13 10:11:13 -0600

    Unhard code the url

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,5 +1,5 @@
 export const fetchSearchQuery = async (search, page) => {
-  const url = `https://api.searchspring.net/api/search/search.json?siteId=${process.env.REACT_APP_SITE_ID}&q=${search}&resultsFormat=native&page=${page}`;
+  const url = `//api.searchspring.net/api/search/search.json?siteId=${process.env.REACT_APP_SITE_ID}&q=${search}&resultsFormat=native&page=${page}`;
   return await fetch(url)
     .then((res) => res.json())
     .then((searchResult) => {
