3ebc773f79abc70a491a5b67182900b655c81ad4
Josh Aragon <josharagon99@gmail.com>
2022-09-13 09:20:41 -0600

    Update fetch to use https

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,5 +1,5 @@
 export const fetchSearchQuery = async (search, page) => {
-  const url = `http://api.searchspring.net/api/search/search.json?siteId=${process.env.REACT_APP_SITE_ID}&q=${search}&resultsFormat=native&page=${page}`;
+  const url = `https://api.searchspring.net/api/search/search.json?siteId=${process.env.REACT_APP_SITE_ID}&q=${search}&resultsFormat=native&page=${page}`;
   return await fetch(url)
     .then((res) => res.json())
     .then((searchResult) => {
@@ -9,7 +9,7 @@ export const fetchSearchQuery = async (search, page) => {
 };
 
 export const fetchSuggestQuery = async (query, page) => {
-  const url = `http://api.searchspring.net/api/suggest/query?siteId=${process.env.REACT_APP_SITE_ID}&query=${query}`;
+  const url = `https://api.searchspring.net/api/suggest/query?siteId=${process.env.REACT_APP_SITE_ID}&query=${query}`;
   return await fetch(url)
     .then((response) => response.json())
     .then((searchResult) => {
