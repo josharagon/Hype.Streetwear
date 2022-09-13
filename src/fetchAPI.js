abbd47f43139defebb080fef6bfc02a54d850131
Josh Aragon <josharagon99@gmail.com>
2022-09-13 11:08:13 -0600

    Access Control Allow Origins

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,7 +1,9 @@
 export const fetchSearchQuery = async (search, page) => {
-  const url = `//api.searchspring.net/api/search/search.json?siteId=${process.env.REACT_APP_SITE_ID}&q=${search}&resultsFormat=native&page=${page}`;
+  const url = `https://api.searchspring.net/api/search/search.json?siteId=${process.env.REACT_APP_SITE_ID}&q=${search}&resultsFormat=native&page=${page}`;
   return await fetch(url, {
-    headers: new Headers({}),
+    headers: new Headers({
+      "Access-Control-Allow-Origin": "*",
+    }),
   })
     .then((res) => res.json())
     .then((searchResult) => {
@@ -10,11 +12,11 @@ export const fetchSearchQuery = async (search, page) => {
     });
 };
 
-export const fetchSuggestQuery = async (query, page) => {
-  const url = `https://api.searchspring.net/api/suggest/query?siteId=${process.env.REACT_APP_SITE_ID}&query=${query}`;
-  return await fetch(url)
-    .then((response) => response.json())
-    .then((searchResult) => {
-      return searchResult;
-    });
-};
+// export const fetchSuggestQuery = async (query, page) => {
+//   const url = `https://api.searchspring.net/api/suggest/query?siteId=${process.env.REACT_APP_SITE_ID}&query=${query}`;
+//   return await fetch(url)
+//     .then((response) => response.json())
+//     .then((searchResult) => {
+//       return searchResult;
+//     });
+// };
